433d0d4183f09422eaf50dfab4631e12ab69516b
jiinja <pianoboy@live.com>
2023-10-22 15:13:18 -0700

    updated UI widget type added gitignore - data too big added code to ingest data

--- main.py
+++ main.py
@@ -1,12 +1,38 @@
 import sys
 from PyQt6.QtWidgets import QApplication, QMainWindow
 from PyQt6 import uic
+import json
 
 
 class MyApp(QMainWindow):
     def __init__(self):
+        # Read business data
+        file = open('./data/yelp_business.json')
+        businessList = []
+        self.loadData(file, businessList)
+
+        # Read checkin data
+        file = open('./data/yelp_checkin.json')
+        checkinList = []
+        self.loadData(file, checkinList)
+
+        # Read review data
+        file = open('./data/yelp_review.json')
+        reviewList = []
+        self.loadData(file, reviewList)
+
+        # Read user data
+        file = open('./data/yelp_user.json')
+        userList = []
+        self.loadData(file, userList)
+
         super().__init__()
         uic.loadUi('myGUI.ui', self)
+
+    def loadData(self, file, list):
+        lines = file.readlines()
+        for line in lines:
+            list.append(json.loads(line))
 
 
 if __name__ == '__main__':
