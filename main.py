bae9ade90a2c93e8300533f2db344e4ff7ef4796
jiinja <pianoboy@live.com>
2023-10-23 20:29:46 -0700

    added code to load cities and zipcodes state selection now limits city options city selection now limits zipcode options

--- main.py
+++ main.py
@@ -13,8 +13,12 @@ class MyApp(QMainWindow):
         self.loadData() 
 
         # Connect data to UI elements
-        self.ui.StateSelector.addItems(self.states)
-        
+        self.ui.StateSelector.addItems(sorted(self.states))
+        self.ui.CitySelector.addItems(sorted(self.cities))
+        self.ui.ZipcodeSelector.addItems(sorted(self.zipcodes))
+
+        self.ui.StateSelector.currentTextChanged.connect(self.updateCitySelector)
+        self.ui.CitySelector.currentTextChanged.connect(self.updateZipcodeSelector)        
 
     # Function to handle loading all data so init is cleaner
     def loadData(self):
@@ -30,7 +34,9 @@ class MyApp(QMainWindow):
         # Read user data
         self.userList = self.loadFile('./data/yelp_user.json')
 
-        self.states = self.loadStates()
+        # Load location selector data
+        self.loadLocations()
+
 
     # Loads data from file (formatted as 1 json object per line) and returns data in a list
     def loadFile(self, file):
@@ -43,12 +49,46 @@ class MyApp(QMainWindow):
         return items
     
     # Iterates through self.businessList and returns a list of unique states
-    def loadStates(self):
-        states = set()
+    def loadLocations(self):
+        self.states, self.cities, self.zipcodes = set(), set(), set()
+        self.statetocity, self.citytozipcode = dict(), dict()
         for business in self.businessList:
-            states.add(business['state'])
-        return list(states)
+            self.states.add(business['state'])
+            self.cities.add(business['city'])
+            self.zipcodes.add(business['postal_code'])
 
+            if business['state'] in self.statetocity:
+                self.statetocity[business['state']].add(business['city'])
+            else:
+                self.statetocity[business['state']] = set()
+                self.statetocity[business['state']].add(business['city'])
+
+            # print(self.statetocity)
+
+            if business['city'] in self.citytozipcode:
+                self.citytozipcode[business['city']].add(business['postal_code'])
+            else:
+                self.citytozipcode[business['city']] = set()
+                self.citytozipcode[business['city']].add(business['postal_code'])
+            # print(self.statetocity)
+
+    def updateCitySelector(self):
+        state = self.ui.StateSelector.currentText()
+        self.ui.ZipcodeSelector.clearSelection()
+        self.ui.ZipcodeSelector.clear()
+        self.ui.CitySelector.clearSelection()
+        self.ui.CitySelector.clear()
+        self.ui.CitySelector.addItems(sorted(self.statetocity[str(state)]))
+
+
+    def updateZipcodeSelector(self):
+        if (self.ui.CitySelector.currentItem()):
+            city = self.ui.CitySelector.currentItem().text()
+            self.ui.ZipcodeSelector.clearSelection()
+            self.ui.ZipcodeSelector.clear()
+            self.ui.ZipcodeSelector.addItems(sorted(self.citytozipcode[str(city)]))
+
+        
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
