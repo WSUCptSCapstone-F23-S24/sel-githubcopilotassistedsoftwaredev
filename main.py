923c2d11c5a58915632a24027d309dbeb4d0f444
jiinja <pianoboy@live.com>
2023-10-23 19:47:20 -0700

    updated gui names restructured data loading added states to state selector

--- main.py
+++ main.py
@@ -6,33 +6,48 @@ import json
 
 class MyApp(QMainWindow):
     def __init__(self):
+        super().__init__()
+        self.ui = uic.loadUi('myGUI.ui', self)
+
+        # Load all data necessary for the application
+        self.loadData() 
+
+        # Connect data to UI elements
+        self.ui.StateSelector.addItems(self.states)
+        
+
+    # Function to handle loading all data so init is cleaner
+    def loadData(self):
         # Read business data
-        file = open('./data/yelp_business.json')
-        businessList = []
-        self.loadData(file, businessList)
+        self.businessList = self.loadFile('./data/yelp_business.json')
 
         # Read checkin data
-        file = open('./data/yelp_checkin.json')
-        checkinList = []
-        self.loadData(file, checkinList)
+        self.checkinList = self.loadFile('./data/yelp_checkin.json')
 
         # Read review data
-        file = open('./data/yelp_review.json')
-        reviewList = []
-        self.loadData(file, reviewList)
+        self.reviewList = self.loadFile('./data/yelp_review.json')
 
         # Read user data
-        file = open('./data/yelp_user.json')
-        userList = []
-        self.loadData(file, userList)
+        self.userList = self.loadFile('./data/yelp_user.json')
 
-        super().__init__()
-        uic.loadUi('myGUI.ui', self)
+        self.states = self.loadStates()
 
-    def loadData(self, file, list):
+    # Loads data from file (formatted as 1 json object per line) and returns data in a list
+    def loadFile(self, file):
+        file = open(file)
         lines = file.readlines()
+        items = []
         for line in lines:
-            list.append(json.loads(line))
+            items.append(json.loads(line))
+        file.close()
+        return items
+    
+    # Iterates through self.businessList and returns a list of unique states
+    def loadStates(self):
+        states = set()
+        for business in self.businessList:
+            states.add(business['state'])
+        return list(states)
 
 
 if __name__ == '__main__':
