6acdb01a465cbe708264052abe73f3bbf92359df
Joshua Burk <joshua.burk@wsu.edu>
2023-11-30 13:17:46 -0800

    Small update to the Location Selector

--- main.py
+++ main.py
@@ -16,16 +16,17 @@ class MyApp(QMainWindow):
         # fill the combo box with states from the states table
         self.comboBox.addItems(Database.get_states(cur))
         self.listWidget.addItems(Database.get_cities(cur, self.comboBox.currentText()))
+
         # fill in the list widget with cities from the cities table of the state selected in the combo box using Database.get_cities that returns a list
         self.comboBox.currentTextChanged.connect(lambda: self.listWidget.clear())
         self.comboBox.currentTextChanged.connect(lambda: self.listWidget.addItems(Database.get_cities(cur, self.comboBox.currentText())))
 
         # fill in the listwidget_2 with zipcodes from the zipcodes table of the city selected in the list widget using Database.get_zipcodes that returns a list
         self.listWidget.currentTextChanged.connect(lambda: self.listWidget_2.clear())
-        self.listWidget.currentTextChanged.connect(lambda: self.listWidget_2.addItems(Database.get_zipcodes(cur, self.listWidget.currentItem().text())))
-
-
-
+        
+        # if listwidget is empty, do nothing
+        # else, fill in listwidget_2 with zipcodes from the zipcodes table of the city selected in the list widget using Database.get_zipcodes that returns a list
+        self.listWidget.currentTextChanged.connect(lambda: self.listWidget_2.addItems(Database.get_zipcodes(cur, self.listWidget.currentItem().text())) if self.listWidget.currentItem() else None)
 
 
 if __name__ == '__main__':
