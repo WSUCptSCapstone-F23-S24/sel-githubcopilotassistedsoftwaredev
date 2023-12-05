a896bde79a2ac42a282f1c0ea25330583660539a
jiinja <joshua.maloy@wsu.edu>
2023-12-05 09:33:33 -0800

    formatting changes updated readme

--- Approach 2/filldatabase.py
+++ Approach 2/filldatabase.py
@@ -25,54 +25,53 @@ userList = loadFile('../Approach 1/yelp_user.json')
 with open('../Approach 1/yelp_zipcode.JSON') as file:
     zipcodeData = json.load(file)
 
-print(len(reviewList))
 
 # Open database connection
-# dbconnection = psycopg2.connect(
-#     host="localhost",
-#     database="postgres",
-#     user="postgres",
-#     password="123")
-# cur = dbconnection.cursor()
+dbconnection = psycopg2.connect(
+    host="localhost",
+    database="postgres",
+    user="postgres",
+    password="123")
+cur = dbconnection.cursor()
 
 
 
 
 # Below code fills zipcode table
-# for zipcode in zipcodeData:
-#      command = f"INSERT INTO zipcode(zipcode, population, avg_income) VALUES ({zipcode['zipcode']}, {zipcode['population']}, {zipcode['meanIncome']});"
-#      cur.execute(command)
+for zipcode in zipcodeData:
+     command = f"INSERT INTO zipcode(zipcode, population, avg_income) VALUES ({zipcode['zipcode']}, {zipcode['population']}, {zipcode['meanIncome']});"
+     cur.execute(command)
 
 
 
 
 # Below code fills business table
-# for business in businessList:
-#     for checkin in checkinList:
-#         if checkin['business_id'] == business['business_id']:
-#             business_checkin = str(checkin['time']).replace("'", "\"")
-#     categories = '['
-#     for category in business['categories']:
-#          categories += f'"{category}",'
-#     categories = categories[:-1] + ']'
-#     categories = categories.replace("'","''")
-#     name = business['name'].replace("'", "''")
-#     address = business['address'].replace("'", "''")
-#     command = f"INSERT INTO business(business_id, categories, city, name, zipcode, review_count, stars, state, checkins, address) VALUES ('{business['business_id']}', '{categories}', '{business['city']}', '{name}', {business['postal_code']}, {business['review_count']}, {business['stars']}, '{business['state']}', '{business_checkin}','{address}');"
-#     cur.execute(command)
+for business in businessList:
+    for checkin in checkinList:
+        if checkin['business_id'] == business['business_id']:
+            business_checkin = str(checkin['time']).replace("'", "\"")
+    categories = '['
+    for category in business['categories']:
+         categories += f'"{category}",'
+    categories = categories[:-1] + ']'
+    categories = categories.replace("'","''")
+    name = business['name'].replace("'", "''")
+    address = business['address'].replace("'", "''")
+    command = f"INSERT INTO business(business_id, categories, city, name, zipcode, review_count, stars, state, checkins, address) VALUES ('{business['business_id']}', '{categories}', '{business['city']}', '{name}', {business['postal_code']}, {business['review_count']}, {business['stars']}, '{business['state']}', '{business_checkin}','{address}');"
+    cur.execute(command)
 
 
 
 
 # Below code fills review table
-# for review in reviewList:
-#      command = f"INSERT INTO review(review_id, business_id, stars, date) VALUES ('{review['review_id']}', '{review['business_id']}', {review['stars']}, '{review['date']}');"
-#      cur.execute(command)
+for review in reviewList:
+     command = f"INSERT INTO review(review_id, business_id, stars, date) VALUES ('{review['review_id']}', '{review['business_id']}', {review['stars']}, '{review['date']}');"
+     cur.execute(command)
 
 
 
 
 # Close database connection
-# cur.close()
-# dbconnection.commit()
-# dbconnection.close()
+cur.close()
+dbconnection.commit()
+dbconnection.close()
--- readme.md
+++ readme.md
@@ -8,10 +8,103 @@
 
 ## Approach 2 Setup 
 
-Approach 2 requires a PostgreSQL installation. After retrieving the json data files, you must create the properly formatted tables using pgadmin4. After that, uncomment the 'filldatabase.py' script and run it. After completing this, Approach 2 will be able to be run.
+Approach 2 requires a PostgreSQL installation. After retrieving the json data files, you must create the properly formatted tables using pgadmin4 or by running the provided create table statements. After that, run the 'filldatabase.py'. After completing this, Approach 2 will be able to be run.
 
 Note: This assumes a complete default PostgreSQL installation, with a password as '123'
 
+## Create Table using CREATE TABLE statements
+### Zipcode Table
+```sql
+-- Table: public.zipcode
+
+-- DROP TABLE IF EXISTS public.zipcode;
+
+CREATE TABLE IF NOT EXISTS public.zipcode
+(
+    zipcode integer NOT NULL,
+    population integer NOT NULL,
+    avg_income integer NOT NULL,
+    CONSTRAINT zipcode_pkey PRIMARY KEY (zipcode)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.zipcode
+    OWNER to postgres;
+
+COMMENT ON TABLE public.zipcode
+    IS 'yelp_zipcode.json
+';
+```
+
+### Business Table
+```sql
+-- Table: public.business
+
+-- DROP TABLE IF EXISTS public.business;
+
+CREATE TABLE IF NOT EXISTS public.business
+(
+    zipcode integer NOT NULL,
+    review_count integer,
+    stars double precision,
+    checkins json,
+    business_id character varying COLLATE pg_catalog."default" NOT NULL,
+    city character varying COLLATE pg_catalog."default" NOT NULL,
+    name character varying COLLATE pg_catalog."default" NOT NULL,
+    state character varying COLLATE pg_catalog."default" NOT NULL,
+    categories json,
+    address character varying COLLATE pg_catalog."default",
+    CONSTRAINT business_pkey PRIMARY KEY (business_id),
+    CONSTRAINT zipcode FOREIGN KEY (zipcode)
+        REFERENCES public.zipcode (zipcode) MATCH FULL
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.business
+    OWNER to postgres;
+
+COMMENT ON TABLE public.business
+    IS 'yelp_business.json file';
+
+COMMENT ON CONSTRAINT zipcode ON public.business
+    IS 'zipcode in this table must match zipcode in zipcode table';
+```
+
+### Review Table
+```sql
+-- Table: public.review
+
+-- DROP TABLE IF EXISTS public.review;
+
+CREATE TABLE IF NOT EXISTS public.review
+(
+    stars integer NOT NULL,
+    date date,
+    business_id character varying COLLATE pg_catalog."default" NOT NULL,
+    review_id character varying COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT review_pkey PRIMARY KEY (review_id),
+    CONSTRAINT business FOREIGN KEY (business_id)
+        REFERENCES public.business (business_id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.review
+    OWNER to postgres;
+
+COMMENT ON TABLE public.review
+    IS 'yelp_review.json';
+```
+
+## Create Table using pgadmin4 
 ### Tables
 #### Business Table
 ![image](./Approach%202/Images/table1.png)
