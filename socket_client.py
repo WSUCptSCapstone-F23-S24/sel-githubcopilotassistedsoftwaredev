67c92ecfb119dda9fd357e9fd12c47e664ff8ebb
Jiinja <pianoboy@live.com>
2024-03-24 13:12:21 -0700

    implemented file sharing

--- socket_client.py
+++ socket_client.py
@@ -1,4 +1,5 @@
 import socket
+import re
 
 
 def client_program():
@@ -12,11 +13,63 @@ def client_program():
     message = input(" -> ")
     
     while message.lower().strip() != 'bye':
-        client_socket.send(message.encode())
+        if(re.search("^Send \".*\"$", message)):
+            # Send file
+            filename = message[message.index('"')+1:-1]
+            try:
+                # Try to open file
+                f = open(filename, 'r')
+                message = "File Name \"" + filename + "\""
+                client_socket.send(message.encode())
+
+                # wait for ack
+                client_socket.recv(1024).decode()
+
+                # Now send file 1024 bytes at a time
+                message = f.read(1024)
+                while(message):
+                    print("Sending " + str(len(message)) + " bytes")
+                    client_socket.send(message.encode())
+
+                    # await ack
+                    client_socket.recv(1024).decode()
+                    message = f.read(1024)
+                
+                # tell server file is done
+                print("Done sending.")
+                message = "Done: \"" + filename + "\""
+                client_socket.send(message.encode())
+
+            except:
+                print("Error: Unable to find file.")
+        else:
+            # Send Message
+            client_socket.send(message.encode())
+
+        # await response
         data = client_socket.recv(1024).decode()
-        
         print('Recieved from server: ' + data)
-        
+
+        # Recieving file
+        if(re.search("^File Name \".*\"$", str(data))):
+
+            # Creating file
+            message = str(data)
+            filename = message[message.index('"')+1:-1]
+            f = open("client_" + filename, 'w')
+            client_socket.send("okay".encode())
+
+            # Recieve file data
+            data = client_socket.recv(1024).decode()
+            while (str(data) != "Done: \"" + filename + "\""):
+                print("Recieving " + str(len(data)) + " bytes")
+                f.write(data)
+                client_socket.send("okay".encode())
+
+                data = client_socket.recv(1024).decode()
+            f.close()
+
+        # get user message from commandline  
         message = input(" -> ")
         
     client_socket.close()
--- socket_server.py
+++ socket_server.py
@@ -1,8 +1,10 @@
 import socket
+import re
+
 
 def server_program():
     host = socket.gethostname()
-    port = 5000 
+    port = 5000
     
     server_socket = socket.socket()
     server_socket.bind((host,port))
@@ -15,10 +17,65 @@ def server_program():
         data = conn.recv(1024).decode()
         if not data:
             break
-        
+            
+        # print data
         print("from connected user: " + str(data))
-        data = input (' -> ')
-        conn.send(data.encode())
+
+        # Handle receiving file
+        if(re.search("^File Name \".*\"$", str(data))):
+
+            # Creating file
+            message = str(data)
+            filename = message[message.index('"')+1:-1]
+            f = open("server_" + filename, 'w')
+            conn.send("okay".encode())
+
+            # recieve file
+            data = conn.recv(1024).decode()
+            while (str(data) != "Done: \"" + filename + "\""):
+                print("Recieving " + str(len(data)) + " bytes")
+                f.write(data)
+                conn.send("okay".encode())
+
+                data = conn.recv(1024).decode()
+            f.close()
+
+        # Get user input
+        message = input (' -> ')
+        
+        # Handle sending file
+        if(re.search("^Send \".*\"$", message)):
+            # Send file
+            filename = message[message.index('"')+1:-1]
+            try:
+                # Try to open file
+                f = open(filename, 'r')
+                message = "File Name \"" + filename + "\""
+                conn.send(message.encode())
+
+                # wait for ack
+                conn.recv(1024).decode()
+
+                # Now send file 1024 bytes at a time
+                message = f.read(1024)
+                while(message):
+                    print("Sending " + str(len(message)) + " bytes")
+                    conn.send(message.encode())
+
+                    # await ack
+                    conn.recv(1024).decode()
+                    message = f.read(1024)
+                
+                # tell server file is done
+                print("Done sending.")
+                message = "Done: \"" + filename + "\""
+                conn.send(message.encode())
+
+            except:
+                print("Error: Unable to find file.")
+        else:
+            # Send Message
+            conn.send(message.encode())
         
     conn.close()
     
